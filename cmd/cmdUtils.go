2a04278b36f6087b7552557c615a641123379922
Patrik Olin <enmejl@gmail.com>
2022-02-01 06:01:50 +0100

    fix faulty monthfolderreplacer

--- cmd/cmdUtils.go
+++ cmd/cmdUtils.go
@@ -6,18 +6,18 @@ import (
 
 func GetMonthFolderReplacer() *strings.Replacer {
 	r := strings.NewReplacer(
-		"January", "01. Januari",
-		"February", "02. Februari",
-		"March", "03. Mars",
-		"April", "04. April",
-		"May", "05. Maj",
-		"June", "06. Juni",
-		"July", "07. Juli",
-		"August", "08. Augusti",
-		"September", "09. September",
-		"October", "10. Oktober",
-		"November", "11. November",
-		"December", "12. December",
+		"January", "01 Januari",
+		"February", "02 Februari",
+		"March", "03 Mars",
+		"April", "04 April",
+		"May", "05 Maj",
+		"June", "06 Juni",
+		"July", "07 Juli",
+		"August", "08 Augusti",
+		"September", "09 September",
+		"October", "10 Oktober",
+		"November", "11 November",
+		"December", "12 December",
 	)
 	return r
 }
